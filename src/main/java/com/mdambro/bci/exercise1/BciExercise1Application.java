e298c0070bd81aa45ca7f28d4cfe8788c9d149c6
Matias DAmbrosio <matias.dambrosio@globallogic.com>
2022-05-23 15:15:49 -0300

    Initial commit.

---
+++ src/main/java/com/mdambro/bci/exercise1/BciExercise1Application.java
@@ -0,0 +1,13 @@
+package com.mdambro.bci.exercise1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BciExercise1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BciExercise1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mdambro/bci/exercise1/BciExercise1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdambro.bci.exercise1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BciExercise1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
